8ca21b6661f8e2ad127465b0e3e5497f00a444e9
Nomeleel <choonlay@outlook.com>
2022-02-24 11:47:30 +0800

    Get AnalysisOptions rules.

---
+++ lib/src/analysis_options/analysis_options.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: implementation_imports
+
+import 'package:analyzer/src/dart/analysis/driver.dart';
+import 'package:analyzer/src/analysis_options/analysis_options_provider.dart';
+import 'package:yaml/yaml.dart';
+
+class AnalysisOptions {
+  const AnalysisOptions({required this.rules});
+
+  factory AnalysisOptions.fromYamlMap(YamlMap? yamlMap) {
+    return AnalysisOptions(rules: yamlMap?['rules'] ?? []);
+  }
+
+  final List<String> rules;
+}
+
+YamlMap? loadByAnalysisDriver(AnalysisDriver driver) {
+  final file = driver.analysisContext?.contextRoot.optionsFile;
+  if (file?.exists ?? false) {
+    return AnalysisOptionsProvider(driver.sourceFactory).getOptionsFromFile(file!)['code_sharp'];
+  }
+}
