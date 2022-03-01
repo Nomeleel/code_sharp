080b15a7d7f45c794723f98d187635e50b5cbd81
Nomeleel <choonlay@outlook.com>
2022-03-01 21:11:02 +0800

    Focus on enabled lint from rules of config.

--- lib/src/linter_options/linter_options.dart
+++ lib/src/linter_options/linter_options.dart
@@ -3,6 +3,8 @@
 import 'package:analyzer/file_system/physical_file_system.dart';
 import 'package:analyzer/src/dart/analysis/driver.dart';
 import 'package:analyzer/src/lint/config.dart';
+import 'package:analyzer/src/lint/registry.dart';
+import 'package:analyzer/src/lint/linter.dart';
 import 'package:code_sharp/src/analysis_options/analysis_options.dart';
 import 'package:linter/src/analyzer.dart';
 
@@ -16,10 +18,19 @@ LinterOptions loadLinterOptions(AnalysisDriver driver) {
   linterOptions.resourceProvider = PhysicalResourceProvider.INSTANCE;
   final analysisOptions = loadAnalysisOptions(driver);
   if (analysisOptions != null) {
-    // TODO(Nomeleel): Modify enabledLints, rules node as enabled lint list.
-    linterOptions.configure(LintConfig.parseMap(analysisOptions));
+    linterOptions.configure2(LintConfig.parseMap(analysisOptions));
   }
   _linterOptionsMap[driver] = linterOptions;
 
   return linterOptions;
+}
+
+extension LinterOptionsExtension on LinterOptions {
+  // Focus on enabled lint from rules of config.
+  void configure2(LintConfig config) {
+    enabledLints = Registry.ruleRegistry.where(
+      (LintRule rule) => config.ruleConfigs.any((rc) => rc.enables(rule.name)),
+    );
+    filter = FileGlobFilter(config.fileIncludes, config.fileExcludes);
+  }
 }
