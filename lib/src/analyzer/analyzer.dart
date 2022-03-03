7a9aa765680df5494e097930c526d5a85154ca6d
Nomeleel <choonlay@outlook.com>
2022-03-03 23:41:16 +0800

    Analysis refine.

--- lib/src/analyzer/analyzer.dart
+++ lib/src/analyzer/analyzer.dart
@@ -4,14 +4,15 @@ import 'dart:io';
 
 import 'package:analyzer/error/error.dart' as error;
 import 'package:analyzer/src/dart/analysis/driver.dart';
+import 'package:analyzer/src/lint/analysis.dart';
+import 'package:analyzer/src/lint/io.dart';
 import 'package:analyzer_plugin/protocol/protocol_common.dart';
 import 'package:code_sharp/src/linter_options/linter_options.dart';
-import 'package:linter/src/analyzer.dart';
 
 Future<Iterable<AnalysisError>> analysis(AnalysisDriver driver, String fileToAnalysis) async {
   final linterOptions = loadLinterOptions(driver);
-  final dartLinter = DartLinter(linterOptions); // TODO(Nomeleel): Reporter imp.
-  final analysisErrorInfo = await lintFiles(dartLinter, [File(fileToAnalysis)]);
+  final lintDriver = LintDriver(linterOptions);
+  final analysisErrorInfo = await lintDriver.analyze([File(fileToAnalysis)].where((f) => isDartFile(f)));
   return analysisErrorInfo.expand((info) => info.errors.map(mapToPluginAnalysisError));
 }
 
