d1287e3ec5cc86479ac8d078e4d56f45e6d95f50
Nomeleel <choonlay@outlook.com>
2022-04-03 18:57:57 +0800

    Remove lint rule sized_box_for_whitespace.

--- lib/src/lint_rule/lint_rule.dart
+++ lib/src/lint_rule/lint_rule.dart
@@ -1,11 +1,9 @@
 import 'package:linter/src/analyzer.dart';
 
-import 'sized_box_for_whitespace.dart';
 import 'style/use_container_property_as_possible.dart';
 
 void registerLintRules() {
   Analyzer.facade.cacheLinterVersion();
   Analyzer.facade
-    ..register(SizedBoxForWhitespace())
     ..register(UseContainerPropertyAsPossible());
 }
